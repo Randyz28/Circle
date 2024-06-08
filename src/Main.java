03e7a9bb2c574b058fa69c1ba5082309f2e4bf9f
randy <randyzam2899@gmail.com>
2024-06-08 18:03:21 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double x = Math.random();
+        double y = Math.random();
+
+        //           (x-0.5)^2 + (y-0.5)^2 <= 0.25
+        // x^2 - x + 0.25 + y^2 - y + 0.25 <= 0.25
+        //              x^2 - x + y^2 - y <= -0.25
+
+        int hit, num;
+
+        //4.0 * hit / num;
+
+        // will have to use for loops and ifs
+    }
+}
